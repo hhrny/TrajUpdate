7ea474245df12abb38ef140eaac286a5df1b7432
hhrny <1016755439@qq.com>
2016-09-06 15:36:23 +0800

    add update trajectory

--- TrajUpdateAlgebra.cpp
+++ TrajUpdateAlgebra.cpp
@@ -235,20 +235,593 @@ struct ConvertMP2UU2Info : OperatorInfo {
 };
 
 
+
+/****************************************************************************
+ 
+   Insert
+ 
+ ***************************************************************************/
+/*
+//
+// UpdateTrajectory
+//
+ListExpr UpdateTrajectoryTypeMap(ListExpr args)
+{
+    NList type(args);
+    if(! type.hasLength(3)){
+        return listutils::typeError("Expecting four arguments");
+    }
+
+    if(! nl->IsEqual(nl->First(args), Upload::BasicType())){
+        return listutlis::typeError("upload unit for first argument expected!");
+    }
+
+    if(! listutils::isRelDescription(nl->Second(args))){
+        return listutils::typeError("relation for second argument expected!");
+    }
+
+    string attrname = type.third().str();
+    ListExpr attrtype = nl->Empty();
+    int j = FindAttribute(nl->Second(nl->Second(args)), attrname, attrtype);
+    
+    if(j != 0){
+        if(nl->SymbolValue(attrtype) != MPoint::BasicType())
+        {
+            return listutils::typeError("first attribute type is not type mpoint");
+        }
+    }else{
+        return listutils::typeError("unknown attribute name " + attrname + " !");
+    }
+    
+    //
+    // create output list
+    ListExpr resulttype = nl->SymbolAtom(CcBool::BasicType());
+    return NList( NList(Symbol::APPEND()),
+            NL->OneElemList(nl->IntAtom(j)),
+            resulttype).listExpr();
+}
+
+//
+//  UpdateTrajectory value map
+//
+int UpdateTrajectoryValueMap(Word *args, Word &result, int message, Word &local, Supplier s)
+{    
+    UploadUnit *unitptr = (UploadUnit*)args[0].addr;
+    Relation *rel = (Relation*)args[1].addr;
+    int attrindex = ((CcInt*)(args[3].addr))->GetValue()-1;
+
+    
+}
+*/
+
+/****************************************************************************
+ 
+   TrajUpdate
+ 
+ ***************************************************************************/
+/*
+TrajUpdate(const ListExpr typeInfo_current_pos, const ListExpr typeInfo_trajectories)
+{
+    // create a new relation of current position and a new relation storing all trajectories of objects
+    // 
+    current_pos = new Relation(typeInfo_current_pos);
+    relDesc_current_pos = current_pos->get
+    trajectories = new Relation(typeInfo_trajectories);
+    
+    // create btree and rtree
+    //
+
+}
+
+TrajUpdate(TupleType *typeInfo_current_pos, TupleType *typeInfo_trajectories)
+{
+
+}
+
+TrajUpdate::TrajUpdate(){
+    cout<<"TrajUpdate::TrajUpdate()"<<endl;
+    // Create SmiUpdateFile
+    suf = new SmiUpdateFile(MyPageSize);
+    suf->Create();
+    fid = suf->GetFileId();
+
+    // Init the TrajUpdate Header
+    header = new TUHeader();
+    size = header->size = 0;
+    pagesize = header->pagesize = MyPageSize;
+
+    // Create the header page
+    SmiUpdatePage *headerpage;
+    int AppendedPage = suf->AppendNewPage(headerpage);
+    assert(AppendedPage);
+    assert(1 == headerpage->GetPageNo());
+
+    UpdateHeader();
+}
+
+TrajUpdate::TrajUpdate(SmiFileId fileid){
+}
+
+TrajUpdate::~TrajUpdate() {
+    cout<<"TrajUpdate::~TrajUpdate"<<endl;
+    //
+}
+
+// The mandatory set of algebra support function
+//
+Word TrajUpdate::In(const ListExpr typeinfo, const ListExpr instance, const int errorPos, ListExpr &errorInfo, bool & correct){
+    cout<<"TrajUpdate::In"<<endl;
+    correct = false;
+    Word result = SetWord(Address(0));
+
+    //check the list length
+    if(nl->ListLength(instance) != 1){
+        cmsg.inFunError("A list of length one expected!");
+        return result;
+    }
+
+    //check string types
+    ListExpr strlist = nl->First(instance);
+    if( !(nl->ListLength(strlist) == 1 &&
+                nl->IsAtom(nl->First(strlist)) &&
+                nl->AtomType(nl->First(strlist)) == StringType))
+    {
+        cmsg.inFunError("A list of one string type values is expected for the first argument");
+        return result;
+    }
+    string str = nl->StringValue(nl->First(strlist));
+    
+    result.addr = new TrajUpdate();
+    ((TrajUpdate*)result.addr)->AddString(str);
+
+    correct = true;
+    return result;
+}
+//
+//
+ListExpr TrajUpdate::Out(ListExpr typeInfo, Word value){
+    cout<<"TrajUpdate::Out"<<endl;
+    TrajUpdate *tu = static_cast<TrajUpdate*>(value.addr);
+    string str = tu->GetString();
+    return nl->OneElemList(nl->StringAtom(str, true));
+}
+//
+//
+Word TrajUpdate::Create(const ListExpr typeinfo){
+    cout<<"TrajUpdate::Create"<<endl;
+    return SetWord(new TrajUpdate());
+}
+//
+//
+void TrajUpdate::Delete(const ListExpr typeinfo, Word &w){
+    cout<<"TrajUpdate::Delete"<<endl;
+    TrajUpdate *tu = static_cast<TrajUpdate *>(w.addr);
+    if(tu->suf->IsOpen()) 
+        tu->suf->Close();
+    delete tu;
+    w.addr = 0;
+}
+//
+//
+bool TrajUpdate::Open(SmiRecord &valueRecord, size_t &offset, const ListExpr typeinfo, Word &value){
+    cout<<"TrajUpdate::Open"<<endl;
+    SmiFileId fileid;
+    //db_pgno_t headerPageNo;
+
+    bool ok = true;
+    ok = ok && valueRecord.Read(&fileid, sizeof(SmiFileId), offset);
+    offset += sizeof(SmiFileId);
+    TrajUpdate *tu = new TrajUpdate(fileid);
+    value.addr = tu;
+    return ok;
+}
+//
+//
+bool TrajUpdate::Save(SmiRecord &valueRecord, size_t &offset, const ListExpr typeinfo, Word &w){
+    cout<<"TrajUpdate::Save"<<endl;
+    TrajUpdate *tu = static_cast<TrajUpdate *>(w.addr);
+    bool ok = true;
+    SmiFileId fileid = tu->fid;
+    ok =  ok && valueRecord.Write(&fileid, sizeof(SmiFileId), offset);
+    offset +=  sizeof(SmiFileId);
+    tu->suf->SyncFile();
+    tu->suf->Close();
+    return ok;
+}
+//
+//
+void TrajUpdate::Close(const ListExpr typeinfo, Word &w){
+    cout<<"TrajUpdate::Close"<<endl;
+    TrajUpdate *tu = static_cast<TrajUpdate *>(w.addr);
+    delete tu;
+    w.addr = 0;
+}
+//
+//
+Word TrajUpdate::Clone(const ListExpr typeinfo, const Word &w){
+    cout<<"TrajUpdate::Clone"<<endl;
+    return SetWord(Address(0));
+}
+//
+//
+bool TrajUpdate::KindCheck(ListExpr type, ListExpr &errorInfo){
+    cout<<"TrajUpdate::KindCheck"<<endl;
+    return (nl->IsEqual(type, TrajUpdate::BasicType()));
+}
+//
+//
+void* TrajUpdate::Cast(void *addr){
+    cout<<"TrajUpdate::Cast"<<endl;
+    return (0);
+}
+//
+//
+int TrajUpdate::SizeOfObj(){
+    cout<<"TrajUpdate::SizeOfObj"<<endl;
+    return sizeof(TrajUpdate);
+}
+//
+//
+ListExpr TrajUpdate::Property(){
+    cout<<"TrajUpdate::Property"<<endl;
+    return nl->TwoElemList(
+                nl->FiveElemList(
+                    nl->StringAtom("Signature"),
+                    nl->StringAtom("Example Type List"),
+                    nl->StringAtom("List Rep"),
+                    nl->StringAtom("Example List"),
+                    nl->StringAtom("Remarks")),
+                nl->FiveElemList(
+                    nl->StringAtom("-> DATA"),
+                    nl->StringAtom(TrajUpdate::BasicType()),
+                    nl->StringAtom("(string)"),
+                    nl->StringAtom("(hello world)"),
+                    nl->StringAtom("a string")));
+}
+//
+//
+*/
+
+/****************************************************************
+
+    1.operator GenUnitTrip
+
+***************************************************************/
+
+// type map function
+// stream(tuple(Uploads uploadunit)) x btree(Id->TupleId) x rel(Id int, pos_x double, pos_y double, timestamp double)
+// -> stream(tuple(Id int, UTrip upoint))
+ListExpr GenUnitTripTM(ListExpr args)
+{
+    ListExpr first_stream, second_btree, third_rel;
+
+    if(nl->ListLength(args) != 3){
+        return listutils::typeError("three arguments expected!");
+    }
+
+    first_stream = nl->First(args);
+    second_btree = nl->Second(args);
+    third_rel = nl->Third(args);
+
+    // check the uploadunit
+    if(! listutils::isTupleStream(first_stream)){
+        return listutils::typeError("first argument should be stream");
+    }
+    ListExpr uu = nl->Second(nl->First(first_stream));
+    if(! listutils::isSymbol(uu, UploadUnit::BasicType())){
+        return listutils::typeError("first argument should be UploadUnit");
+    }
+
+    // check the btree
+    if( nl->IsAtom(second_btree)
+            || (nl->ListLength(second_btree) != 3)){
+        return listutils::typeError("second argument should be btree");
+    }
+    ListExpr btree_symbol = nl->First(second_btree);
+    if(! nl->IsAtom(btree_symbol)
+            || (nl->AtomType(btree_symbol) != SymbolType)
+            || (nl->SymbolValue(btree_symbol) != BTree::BasicType())){
+        return listutils::typeError("second argument should be btree");
+    }
+
+    // check the relation of current position
+    if(! listutils::isRelDescription(third_rel)){
+        return listutils::typeError("third argument should be relation");
+    }
+
+    //
+    //
+    //
+    //
+    //
+    return nl->Empty();
+}
+
+//value map function
+//
+int GenUnitTripVM(Word* args, Word& result, int message, Word& local, Supplier s)
+{
+
+    switch( message )
+    {
+        case OPEN:
+            {
+            }
+        case REQUEST:
+            {
+            }
+        case CLOSE:
+            {
+                return 0;
+            }
+    }
+    return 0;
+}
+
+//
+// operator info
+struct GenUnitTripInfo : OperatorInfo {
+    GenUnitTripInfo()
+    {
+        name      = "genunittrip";
+        signature = "((stream (tuple([a1:d1, ..., ai:int, ..., "
+            "aj:uploaduint, ..., an:dn]))) x btree "
+            "x rel( tuple( b1:int, b2:double, b3:double, b4:double)))"
+            " -> stream (tuple (Id int, Utrip upoint))";
+        syntax    = "_ genunittrip [ _, _ ]";
+        meaning   = "generate a unit trip of type upoint from upload unit,"
+            "btree and relation of current moving object position";
+    }
+};
+
+
+/****************************************************************
+
+    operator UpdateTrajectory
+
+***************************************************************/
+
+// type map function
+// stream(tuple(Id int, UTrip upoint)) x rel(tuple(Id int, Trip mpoint)
+// x btree x rtree x Id x UTrip x Trip
+// -> bool
+ListExpr UpdateTrajectoryTM(ListExpr args)
+{
+    ListExpr first, second, third, fourth, fifth, sixth, seventh;
+    int i1, i2, i3, i4; //i1: Id, i2: UTrip, i3:Id, i4:Trip
+    string attrname;
+    ListExpr attrtype, attrlist;
+
+    // check the number of arguments
+    if(nl->ListLength(args) != 7){
+        listutils::typeError("Seven arguments expected!");
+    }
+
+    //
+    first = nl->First(args);      //stream
+    second = nl->Second(args);      //relation
+    third = nl->Third(args);        //btree
+    fourth = nl->Fourth(args);      //rtree
+    fifth = nl->Fifth(args);     //Id
+    sixth = nl->Sixth(args);   //UTrip
+    seventh = nl->Seventh(args);   //Trip
+
+    //
+    //
+    // check the stream
+    if(! listutils::isTupleStream(first)){
+        return listutils::typeError("First argument should be stream!");
+    }
+
+    // check the relation
+    if(! listutils::isRelDescription(second)){
+        return listutils::typeError("Second argument should be relation!");
+    }
+
+    // check the btree
+    if(nl->IsAtom(third) || nl->ListLength(third) != 3){
+        return listutils::typeError("Third argument should be btree!");
+    }
+    ListExpr btreeSymbol = nl->First(third);
+    if(! nl->IsAtom(btreeSymbol) 
+            || (nl->AtomType(btreeSymbol) != SymbolType)
+            || (nl->SymbolValue(btreeSymbol) != BTree::BasicType())){
+        return listutils::typeError("Third argument should be btree!");
+    }
+
+    // check the rtree
+    if(! listutils::isRTreeDescription(fourth)){
+        return listutils::typeError("Fourth argument should be rtree!");
+    }
+    ListExpr rtreeKeyType = listutils::getRTreeType(fourth);
+    if((! listutils::isSpatialType(rtreeKeyType)) && (! listutils::isRectangle(rtreeKeyType))){
+        return listutils::typeError("Rtree not over a spatial attribute!");
+    }
+    ListExpr rtreeTupleDesc = nl->Second(fourth);
+    if(! nl->Equal(rtreeTupleDesc, nl->Second(second))){
+        return listutils::typeError("Type of rtree and  relation are different!");
+    }
+    // get the index of Id, UTrip and Trip
+    // get the index of Id in stream
+    attrname = nl->SymbolValue(fifth);
+    attrtype = nl->Empty();
+    attrlist = nl->Second(nl->Second(first));
+    i1 = FindAttribute(attrlist, attrname, attrtype);
+    if(i1 == 0){
+        return listutils::typeError("Attribute name '"+attrname+"' is not know!");
+    }else{
+        if(nl->SymbolValue(attrtype) != CcInt::BasicType()){
+            return listutils::typeError("Attribute type is not of type int!");
+        }
+    }
+    // get the index of UTrip in stream
+    attrname = nl->SymbolValue(sixth);
+    attrtype = nl->Empty();
+    i2 = FindAttribute(attrlist, attrname, attrtype);
+    if(i2 == 0){
+        return listutils::typeError("Attribute name '"+attrname+"' is not know!");
+    }else{
+        if(nl->SymbolValue(attrtype) != UPoint::BasicType()){
+            return listutils::typeError("Attribute type is not of type upoint!");
+        }
+    }
+    // get the index of Id in relation
+    attrname = nl->SymbolValue(fifth);
+    attrtype = nl->Empty();
+    attrlist = nl->Second(nl->Second(second));
+    i3 = FindAttribute(attrlist, attrname, attrtype);
+    if(i3 == 0){
+        return listutils::typeError("Attribute name '"+attrname+"' is not know!");
+    }else{
+        if(nl->SymbolValue(attrtype) != CcInt::BasicType()){
+            return listutils::typeError("Attribute type is not of type int!");
+        }
+    }
+    // get the index of Trip in relation
+    attrname = nl->SymbolValue(seventh);
+    attrtype = nl->Empty();
+    i4 = FindAttribute(attrlist, attrname, attrtype);
+    if(i4 == 0){
+        return listutils::typeError("Attribute name '"+attrname+"' is not know!");
+    }else{
+        if(nl->SymbolValue(attrtype) != MPoint::BasicType()){
+            return listutils::typeError("Attribute type is not of type mpoint!");
+        }
+    }
+    // construct a result type, combine the result and attribute index    
+    ListExpr resulttype = nl->SymbolAtom("bool");
+
+    return nl->ThreeElemList(
+            nl->SymbolAtom(Symbol::APPEND()),
+            nl->FourElemList(
+                nl->IntAtom(i1),
+                nl->IntAtom(i2),
+                nl->IntAtom(i3),
+                nl->IntAtom(i4)),
+            resulttype);
+}
+
+
+//
+// get tuple id
+//
+TupleId getTupleId(CcInt *id, BTree *btree)
+{
+    BTreeIterator *it;
+    //TupleId tid;
+    assert(id->IsDefined());
+    assert(btree);
+
+    it = btree->ExactMatch(id);
+
+    if(it == 0){
+        return -1;
+    }
+    return it->GetId();
+}
+
+//value map function
+//
+// stream(tuple(Id int, UTrip upoint)) x rel(tuple(Id int, Trip mpoint)
+// x btree x rtree x Id x UTrip x Trip
+// -> bool
+int UpdateTrajectoryVM(Word* args, Word& result, int message, Word& local, Supplier s)
+{
+    Relation *rel;
+    R_Tree<3, TupleId> *rtree;
+    BTree    *btree;
+    Word     elem;
+    Tuple    *tuple_old, *tuple_new, *tuple_stream, *tuple_rel;
+    int      iid1, iutrip, iid2, itrip;
+    CcInt    *vid;
+    TupleId  tid;
+    UPoint   *up;
+    MPoint   *mp;
+    vector<int> *changedIndeices = NULL;
+    vector<Attribute *> *newAttrs = NULL;
+
+    // get the local addr
+
+    rel = (Relation *)args[1].addr;
+    btree = (BTree *)args[2].addr;
+    rtree = (R_Tree<3, TupleId> *)args[3].addr;
+    iid1 = ((CcInt *)args[7].addr)->GetIntval() - 1;
+    iutrip = ((CcInt *)args[8].addr)->GetIntval() - 1;
+    iid2 = ((CcInt *)args[9].addr)->GetIntval() - 1;
+    itrip = ((CcInt *)args[10].addr)->GetIntval() - 1;
+
+    changedIndeices = new vector<int>(1);
+    newAttrs = new vector<Attribute *>(1);
+    //
+    qp->Open(args[0].addr);
+    qp->Request(args[0].addr, elem);
+    while(qp->Received(args[0].addr)){
+        tuple_stream = (Tuple *)elem.addr;
+        vid = (CcInt *)tuple_stream->GetAttribute(iid1);
+        up = ((UPoint *)tuple_stream->GetAttribute(iutrip))->Clone();
+
+        // get the tupleid of object in relation, using btree
+        tid = getTupleId(vid, btree);   
+        // get the tuple storing the trajectory of object
+        tuple_rel = rel->GetTuple(tid, true);
+        mp = (MPoint *)tuple_rel->GetAttribute(itrip)->Clone();
+        // update the trajectory
+        mp->StartBulkLoad();
+        mp->MergeAdd(*up);
+        mp->EndBulkLoad();
+        
+        // save the change to relation
+        (*changedIndeices)[0] = itrip;
+        (*newAttrs)[0] = mp;
+        rel->UpdateTuple(tuple_rel, *changedIndeices, *newAttrs);
+
+        // update the rtree
+        // very important
+        
+        // get next tuple in stream
+        qp->Request(args[0].addr, elem);
+    }
+    qp->Close(args[0].addr);
+    delete changedIndeices;
+    delete newAttrs;
+    result.addr = new CcBool(true);
+    return 0;
+}
+
+//
+// operator info
+struct UpdateTrajectoryInfo : OperatorInfo {
+    UpdateTrajectoryInfo()
+    {
+        name      = "updatetrajectory";
+        signature = "((stream (tuple([Id:int, UTrip:upoint])))"
+            "(rel(tuple(Id:int, Trip:mpoint))) x btree x rtree x Id x UTrip x Trip)"
+            " -> stream (tuple (Upload uploadunit))";
+        syntax    = "_ updatetrajectory [ _, _, _, _, _, _]";
+        meaning   = "add a unit trajectory to trajectories";
+    }
+};
+
+/****************************************************************************
+ 
+   TrajUpdateAlgebra
+ 
+ ***************************************************************************/
+
 class TrajUpdateAlgebra : public Algebra
 {
 public:
     TrajUpdateAlgebra() : Algebra()
     {
         AddOperator( ConvertMP2UU2Info(), ConvertMP2UU2VM, ConvertMP2UU2TM );
+        AddOperator( UpdateTrajectoryInfo(), UpdateTrajectoryVM, UpdateTrajectoryTM );
     }
 };
 
 
-
 /******************************************************************************
 
-  15 Initialization of SETIAlgebra
+  Initialization of SETIAlgebra
 
  ******************************************************************************/
 
--- TrajUpdateAlgebra.h
+++ TrajUpdateAlgebra.h
@@ -1,10 +1,87 @@
 #ifndef _TRAJUPDATEALGEBRA_H
 #define _TRAJUPDATEALGEBRA_H
 
+#include "NestedList.h"
+#include "ListUtils.h"
+#include <map>
+#include <list>
+#include <string>
+/*
+//
+//
+class TrajUpdate
+{
+public:
+    // create a new Trajectory Update
+    // give the two tuple type information of current position relation and trajectories relation;
+    //
+    TrajUpdate(const ListExpr typeInfo_current_pos, const ListExpr typeInfo_trajectories); 
+    TrajUpdate(TupleType *typeInfo_current_pos, TupleType *typeInfo_trajectories);
+    
+
+    // open a Trajectory Update
+    TrajUpdate(SmiFileId fileid_btree, SmiFileId fileid_rtree, 
+               RelationDescriptor &relDesc_current_pos, RelationDescriptor &relDesc_trajectories);
+    
+    // destructor a Trajectory Update
+    ~TrajUpdate();
+
+    // The mandatory set of algebra support function
+    //
+    static Word     In(const ListExpr typeinfo, const ListExpr instance,
+                       const int errorPos, ListExpr &errorInfo, bool & correct);
+    static ListExpr Out(ListExpr typeInfo, Word value);
+    static Word     Create(const ListExpr typeinfo);
+    static void     Delete(const ListExpr typeinfo, Word &w);
+    static bool     Open(SmiRecord &valueRecord, size_t &offset,
+                         const ListExpr typeinfo, Word &value);
+    static bool     Save(SmiRecord &valueRecord, size_t &offset,
+                         const ListExpr typeinfo, Word &w);
+    static void     Close(const ListExpr typeinfo, Word &w);
+    static Word     Clone(const ListExpr typeinfo, const Word &w);
+    static bool     KindCheck(ListExpr type, ListExpr &errorInfo);
+    static void*    Cast(void *addr);
+    static int      SizeOfObj();
+    static ListExpr Property();
+
+    //
+    inline static const string BasicType() { return "trajupdate"; }
+
+    //
+    static const bool checkType(const ListExpr type) {
+        return listutils::isSymbol(type, BasicType());
+    }
+
+private:
+    //SmiRecordFile* rtreefile;
+    //SmiFileId fid;
+    
+    //file identifier of btree
+    SmiFileId fidofbtree;
+    //file identifier of rtree
+    SmiFileId fidofrtree;
+
+    // btree to index the identifier of objects
+    BTree btree;
+    // 3d rtree to index the trjecotries using MBR of trajectory
+    R_Tree rtree;
+
+    //Relation of current position  (Id(int), Pos_x(double), Pos_y(double), Timestamp(double))
+    Relation current_pos;
+
+    //Relation of trajectories  (Id(int), Traj(mpoint))
+    Relation trajectories;
+
+    //
+    // restore the descriptor of relation
+    RelationDescriptor relDesc_current_pos;
+    RelationDescriptor relDesc_trajectories;
 
 
-
-
+    //size_t size;
+    //size_t pagesize;
+};
+*/
 
 #endif
 
